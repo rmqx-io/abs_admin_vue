1a81d41a88f78d649214a684627661383f2d7c03
Zhang Hu <iamzhanghu@gmail.com>
2023-03-06 14:56:46 +0800

    fallback to locale zhCN (for a-date-picker)

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,14 @@
 <script>
 import { domTitle, setDocumentTitle } from '@/utils/domUtil'
 import { i18nRender } from '@/locales'
+import zhCN from 'ant-design-vue/es/locale/zh_CN'
+import moment from 'moment'
+import 'moment/locale/zh-cn'
 
 export default {
   data () {
     return {
+      // locale: zhCN
     }
   },
   computed: {
@@ -21,7 +25,13 @@ export default {
       const { title } = this.$route.meta
       title && (setDocumentTitle(`${i18nRender(title)} - ${domTitle}`))
 
-      return this.$i18n.getLocaleMessage(this.$store.getters.lang).antLocale
+      const locale = this.$i18n.getLocaleMessage(this.$store.getters.lang).antLocale
+      console.log('locale', locale)
+      if (locale === undefined) {
+        console.log('locale fallback to zhCN')
+        return zhCN
+      }
+      return locale
     }
   }
 }
