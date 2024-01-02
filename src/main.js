f2616142be244d71438e4210ab2b5fa818aa8df1
Zhang Hu <iamzhanghu@gmail.com>
2024-01-02 21:14:08 +0800

    amap key

--- src/main.js
+++ src/main.js
@@ -59,7 +59,8 @@ AmapVue.config.version = '2.0' // 默认2.0，这里可以不修改
 // AmapVue.config.key = '5817d32d7eb114d90df69cbf22cf1c87'
 // AmapVue.config.key = '5817d32d7eb114d90df69cbf22cf1c87'
 // AmapVue.config.key = 'c5329a28ee7609e3832c02bb6317aff8'
-AmapVue.config.key = 'e13456422e8fe93451cf2201f4db84bd'
+// AmapVue.config.key = 'e13456422e8fe93451cf2201f4db84bd'
+AmapVue.config.key = '977ce14bef9c9121ce037c5dfaa2db42'
 AmapVue.config.plugins = [
   'AMap.Autocomplete',
   'AMap.PlaceSearch',
