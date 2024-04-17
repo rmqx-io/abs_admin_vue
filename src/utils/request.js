b12a7b8c8c7a05fe4dd4db514066593559180130
Zhang Hu <iamzhanghu@gmail.com>
2024-04-17 14:49:50 +0800

    修复导出设备列表超时

--- src/utils/request.js
+++ src/utils/request.js
@@ -10,7 +10,7 @@ import {VUE} from '@/main'
 const request = axios.create({
   // API 请求的默认前缀
   baseURL: process.env.VUE_APP_API_BASE_URL,
-  timeout: 30000 // 请求超时时间
+  timeout: 0 // 请求超时时间
 })
 
 const request_no_timeout = axios.create({
