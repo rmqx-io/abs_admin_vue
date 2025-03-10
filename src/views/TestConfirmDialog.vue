6e1647e4fbe3f75ef30c2ad6c9e083d6d64d19cd
Zhang Hu <iamzhanghu@gmail.com>
2025-03-10 12:22:16 +0800

    confirm dialog examples

---
+++ src/views/TestConfirmDialog.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="test-confirm-dialog">
+    <h1>Confirm Dialog Test Page</h1>
+    
+    <div class="test-controls">
+      <el-button type="primary" @click="showBasicConfirm">Basic Confirmation</el-button>
+      <el-button type="warning" @click="showWarningConfirm">Warning Confirmation</el-button>
+      <el-button type="danger" @click="showDangerConfirm">Danger Confirmation</el-button>
+      <el-button type="info" @click="showCustomConfirm">Custom Confirmation</el-button>
+    </div>
+
+    <div class="result-panel" v-if="lastResult !== null">
+      <h3>Last Result:</h3>
+      <pre>{{ lastResult }}</pre>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestConfirmDialog',
+  data() {
+    return {
+      lastResult: null
+    }
+  },
+  methods: {
+    showBasicConfirm() {
+      this.$confirm('Are you sure you want to proceed?', 'Confirmation', {
+        confirmButtonText: 'OK',
+        cancelButtonText: 'Cancel',
+        type: 'info'
+      }).then(() => {
+        this.lastResult = 'User confirmed the basic dialog'
+      }).catch(() => {
+        this.lastResult = 'User cancelled the basic dialog'
+      })
+    },
+    
+    showWarningConfirm() {
+      this.$confirm('This action may have consequences. Continue?', 'Warning', {
+        confirmButtonText: 'Continue',
+        cancelButtonText: 'Cancel',
+        type: 'warning'
+      }).then(() => {
+        this.lastResult = 'User confirmed the warning dialog'
+      }).catch(() => {
+        this.lastResult = 'User cancelled the warning dialog'
+      })
+    },
+    
+    showDangerConfirm() {
+      this.$confirm('This action cannot be undone! Are you sure?', 'Danger', {
+        confirmButtonText: 'Delete',
+        cancelButtonText: 'Cancel',
+        type: 'error'
+      }).then(() => {
+        this.lastResult = 'User confirmed the danger dialog'
+      }).catch(() => {
+        this.lastResult = 'User cancelled the danger dialog'
+      })
+    },
+    
+    showCustomConfirm() {
+      this.$confirm('Would you like to save your changes before continuing?', 'Custom Confirmation', {
+        confirmButtonText: 'Save',
+        cancelButtonText: 'Don\'t Save',
+        distinguishCancelAndClose: true,
+        showClose: true,
+        type: 'info',
+        center: true
+      }).then(() => {
+        this.lastResult = 'User clicked Save'
+      }).catch((action) => {
+        if (action === 'cancel') {
+          this.lastResult = 'User clicked Don\'t Save'
+        } else {
+          this.lastResult = 'User closed the dialog'
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.test-confirm-dialog {
+  padding: 20px;
+}
+
+.test-controls {
+  margin: 20px 0;
+  display: flex;
+  gap: 10px;
+  flex-wrap: wrap;
+}
+
+.result-panel {
+  margin-top: 20px;
+  padding: 15px;
+  background-color: #f5f7fa;
+  border-radius: 4px;
+}
+
+pre {
+  white-space: pre-wrap;
+  word-break: break-all;
+}
+</style>
