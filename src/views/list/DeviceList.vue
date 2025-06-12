a1e074451db766ae0119fd790d79ecebcea45c8a
Zhang Hu <iamzhanghu@gmail.com>
2025-06-12 16:33:40 +0800

    Update DeviceList component to adjust SOC calculation based on new BMS type
    - Modify the SOC display logic to reflect changes for BMS type 236
    - Ensure accurate representation of state of charge for different device types

--- src/views/list/DeviceList.vue
+++ src/views/list/DeviceList.vue
@@ -194,7 +194,8 @@
 
         <span slot="bms_info" slot-scope="text, record">
           <template>
-            SOC: <span>{{ record.bms_type === 227 ? record.bms_soc : record.bms_soc / 50 }}</span>
+            <!-- SOC: <span>{{ record.bms_type === 227 ? record.bms_soc : record.bms_soc / 50 }}</span> -->
+             SOC: <span>{{ record.bms_type === 236 ? record.bms_soc / 50 : record.bms_soc }}</span>
             <br />
             SOH: <span>{{ record.bms_soh }}</span>
             <br />
